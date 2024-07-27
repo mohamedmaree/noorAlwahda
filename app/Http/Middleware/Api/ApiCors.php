832258f614b0f3008d45a896c3fae56c73106a90
mohamed maree <m7mdmaree26@gmail.com>
2024-07-27 03:06:55 +0300

    api cors Content-Type with flutter

--- app/Http/Middleware/Api/ApiCors.php
+++ app/Http/Middleware/Api/ApiCors.php
@@ -11,8 +11,8 @@ class ApiCors {
       "Access-Control-Allow-Headers" => "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method",
       "Access-Control-Allow-Methods" => "POST, GET, OPTIONS, PUT, DELETE",
 
-      "Content-Type"                 => "application/json,form-data,charset=UTF-8", // use this with android and ios
-      // "Content-Type"              => "application/json", // use this with flutter
+      // "Content-Type"                 => "application/json,form-data,charset=UTF-8", // use this with android and ios
+      "Content-Type"              => "application/json", // use this with flutter
 
       "Access-Control-Max-Age"       => "1000",
     ];
