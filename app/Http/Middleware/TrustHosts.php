a521a87346ab12e4a8610605fbd702115b2cb4d7
mohamed maree <m7mdmaree26@gmail.com>
2024-09-25 01:48:19 +0300

    add domain to trust hosts

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,7 +15,7 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
-            'https://nooralwahdah.com',
+            'nooralwahdah.com',
         ];
     }
 }
